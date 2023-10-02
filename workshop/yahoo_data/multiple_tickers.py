9772772ffd94e62a7c23916ae12a70d822a7b17b
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-10-02 10:54:45 -0400

    lgtm

---
+++ workshop/yahoo_data/multiple_tickers.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import yfinance as yf
+
+data = yf.download('SPY COST', start='2023-09-02', end='2023-10-02')
+# -
+type(data)
+
+data
+
+
